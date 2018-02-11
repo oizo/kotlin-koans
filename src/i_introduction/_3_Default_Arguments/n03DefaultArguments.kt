42f8105d8c8814a79110493289cab5703e024dca
Danny Hvam <dkhvam@gmail.com>
2018-02-12 00:47:15 +0100

    web version, and git repo not exactly the same, remember to run tests locally

--- src/i_introduction/_3_Default_Arguments/n03DefaultArguments.kt
+++ src/i_introduction/_3_Default_Arguments/n03DefaultArguments.kt
@@ -14,7 +14,7 @@ fun todoTask3(): Nothing = TODO(
     documentation = doc2(),
     references = { name: String -> JavaCode3().foo(name); foo(name) })
 
-fun foo(name: String, number: Int = 0, toUpperCase: Boolean = false): String =
+fun foo(name: String, number: Int = 42, toUpperCase: Boolean = false): String =
     (if (toUpperCase) name.toUpperCase() else name) + number
 
 
